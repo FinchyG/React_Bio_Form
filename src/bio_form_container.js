372325fa209b7076edc166cf539d16121cac860f
FinchyG <lagaunt120@yahoo.com>
2018-07-07 14:44:35 +0100

    + working fav_col and interests functions

--- src/bio_form_container.js
+++ src/bio_form_container.js
@@ -16,7 +16,11 @@ export default class Bio_Form extends React.Component {
             gender_prefix: "",
             gender_prefix_valid: false,
             age: "",
-            age_valid: false
+            age_valid: false,
+            fav_col: "",
+            fav_col_valid: false,
+            interests: "",
+            interests_valid: false
         }
 
         this.process_data = this.process_data.bind(this);
@@ -24,6 +28,8 @@ export default class Bio_Form extends React.Component {
         this.get_last_name = this.get_last_name.bind(this);
         this.get_gender_prefix = this.get_gender_prefix.bind(this);
         this.get_age = this.get_age.bind(this);
+        this.get_fav_col = this.get_fav_col.bind(this);
+        this.get_interests = this.get_interests.bind(this);
 
     }
 
@@ -33,6 +39,8 @@ export default class Bio_Form extends React.Component {
         this.get_last_name();
         this.get_gender_prefix();
         this.get_age();
+        this.get_fav_col();
+        this.get_interests();
 
     }
 
@@ -124,6 +132,93 @@ export default class Bio_Form extends React.Component {
 
     }
 
+    get_fav_col() {
+
+        const fav_color = document.getElementById("fav_col").value;
+
+        if(fav_color === "") {
+
+            document.getElementById("fav_color_warn").style.visibility = "visible";
+            this.setState((fav_col_valid) => ({fav_col_valid: false}));
+
+        } else {
+
+            document.getElementById("fav_color_warn").style.visibility = "hidden";
+            this.setState((fav_col_valid) => ({fav_col_valid: true}));
+            this.setState((fav_col) => ({fav_col: fav_color}));
+
+        }
+    }
+
+    get_interests() {
+
+        const interests_arr = [
+            document.getElementById("sports"),
+            document.getElementById("music"),
+            document.getElementById("reading"),
+            document.getElementById("gardening"),
+            document.getElementById("cookery"),
+            document.getElementById("D.I.Y")        
+        ];
+    
+        let sel_interests_arr = [];
+    
+        for(let i=0; i < interests_arr.length; i++) {
+    
+            if(interests_arr[i].checked) {
+                sel_interests_arr.push(interests_arr[i].value);
+            }
+    
+        }
+    
+    
+    
+        if(sel_interests_arr.length === 0) {
+    
+            document.getElementById("interests_warn").style.visibility = "visible";
+            this.setState((interests_valid) => ({interests_valid: false}));
+    
+        }
+    
+        let loop_interests_str = "";
+        let interests_str = "";
+    
+        if(sel_interests_arr.length >= 3) {
+    
+            for(let i=0; i < sel_interests_arr.length - 2; i++) {
+    
+                loop_interests_str += sel_interests_arr[i] + ", ";
+            
+            }
+    
+            interests_str += loop_interests_str + sel_interests_arr[sel_interests_arr.length -2] + " and " + 
+                             sel_interests_arr[sel_interests_arr.length -1];
+            
+            this.setState((interests_valid) => ({interests_valid: true}));
+        
+        }
+    
+        if(sel_interests_arr.length === 2) {
+    
+            interests_str += sel_interests_arr[sel_interests_arr.length -2] + " and " + 
+                             sel_interests_arr[sel_interests_arr.length -1];
+    
+                             this.setState((interests_valid) => ({interests_valid: true}));
+    
+        }
+    
+        if(sel_interests_arr.length === 1) {
+    
+            interests_str += sel_interests_arr;
+    
+            this.setState((interests_valid) => ({interests_valid: true}));;
+    
+        }
+    
+        this.setState((interests) => ({interests: interests_str}));
+        
+    }
+
     render() {
 
         return (
@@ -160,6 +255,32 @@ export default class Bio_Form extends React.Component {
                     <span className="warning" id="age_warn">*Enter your age in years with a number*</span>
                 </div>
 
+                <div className="inner_div">
+                    <label>Favourite Colour: </label>
+                    <select id="fav_col">
+                        <option value="" selected disabled></option>
+                        <option value="red">Red</option>
+                        <option value="orange">Orange</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="green">Green</option>
+                        <option value="blue">Blue</option>
+                        <option value="white">White</option>
+                        <option value="black">Black</option>                
+                    </select>
+                    <span className="warning" id="fav_color_warn">*Select your favourite color*</span>
+                </div>
+
+                <div className="inner_div">
+                    <label>Interests:</label>
+                        <label>Sports: </label><input type="checkbox" id="sports" name="interests" value="sports" />
+                        <label>Music: </label><input type="checkbox" id="music" name="interests" value="music" />
+                        <label>Reading: </label><input type="checkbox" id="reading" name="interests" value="reading" />
+                        <label>Gardening: </label><input type="checkbox" id="gardening" name="interests" value="gardening" />
+                        <label>Cookery: </label><input type="checkbox" id="cookery" name="interests" value="cookery" />
+                        <label>D.I.Y: </label><input type="checkbox" id="D.I.Y" name="interests" value="D.I.Y" />
+                    <span className="warning" id="interests_warn">*Select at least one interest*</span>
+                </div>
+
                 <div>
                 <Submit_Button onClick={this.process_data} />
                 </div>
@@ -168,7 +289,9 @@ export default class Bio_Form extends React.Component {
                     <Bio_Para_One first_name={this.state.first_name} first_name_valid={this.state.first_name_valid} 
                               last_name={this.state.last_name} last_name_valid={this.state.last_name_valid} 
                               gender_prefix={this.state.gender_prefix} gender_prefix_valid={this.state.gender_prefix_valid}
-                              age={this.state.age} age_valid={this.state.age_valid} />
+                              age={this.state.age} age_valid={this.state.age_valid} 
+                              fav_col={this.state.fav_col} fav_col_valid={this.state.fav_col_valid} 
+                              interests={this.state.interests} interests_valid={this.state.interests_valid} />
                 </div>
                 
             </div>
--- src/bio_para_one.js
+++ src/bio_para_one.js
@@ -4,9 +4,10 @@ export const Bio_Para_One = (props) => {
 
     
     if((props.first_name_valid === true) && (props.last_name_valid === true) && (props.gender_prefix_valid === true) &&
-        (props.age_valid === true))  {
+        (props.age_valid === true) && (props.fav_col_valid === true) && (props.interests_valid === true))  {
 
-        return <p>My name is {props.gender_prefix} {props.first_name} {props.last_name}, and I am {props.age} years old.</p>;
+        return <p>My name is {props.gender_prefix} {props.first_name} {props.last_name}, and I am {props.age} years old. 
+                My favourite color is {props.fav_col}, and I am interested in {props.interests}.</p>;
 
     }
 
